0a402513e3a1d94c88562ee125ffff89dbdf5517
welisten <welistenw@gmail.com>
2025-04-10 04:04:43 -0300

    DGAS - class User updated

--- src/games/g3/src/Class/User.js
+++ src/games/g3/src/Class/User.js
@@ -1,16 +1,29 @@
+/**
+ * @class User
+ * @classdesc Representa um jogador na aplicação.
+ * 
+ * Essa classe gerencia os dados do jogador, incluindo nome, tesouros e nível.
+ * Também fornece métodos para atualizar o progresso do jogador e penalizá-lo
+ * caso ele reinicie o jogo. Pode ser estendida no futuro para incluir atributos privados
+ * e lógica adicional conforme o jogo evoluir.
+ */
+
 class User {
     constructor(nome){
         this.name = nome
         this.treasures = 0
         this.level = 0
     }
+
     // os getter e setter foram construidos para o meomento onde atributos serão privados
-    getNome(){
-        return this.nome
+    getName(){
+        return this.name
     }
+
     getTreasures(){
         return this.treasures
     }
+
     getLevel(){
         return this.level
     }
@@ -18,20 +31,26 @@ class User {
     setName(name){
         this.name = name
     }
+
     setLevel(level){
-        this.level = level
+        if(typeof level === 'number' && level >= 0){
+            this.level = level
+        } else {
+            throw new Error("Nível inválido!")
+        }
     }
     
     updateTreasures(){
         this.treasures++
     }
+
     updateUser(){
         this.level++
     }
 
     replayUserGame(){
-        this.treasures -= 8
-        // this.level -= 1  LIBERAR APENAS QUANDO HOUVER MAIS DE UMA FASE
+        this.treasures = Math.max(0, this.treasures - 8)
+        if(this.level > 1) this.level--
     }
 }
 
